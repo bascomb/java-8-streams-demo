45eacc4e0bc72e178c5ef34681dd83ae4191841c
Bascomb Arnette <bascomb.professional@gmail.com>
2019-10-21 18:01:32 -0700

    sort, filter list of dog heights

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # java-8-streams-demo
+Demo Java 8 Streams:
+Create a Collection of dog heights.
+Print all heights.
+Print all heights less than 16.
+Convert all heights to meters.
--- src/main/java/com/bascomb/App.java
+++ src/main/java/com/bascomb/App.java
@@ -1,5 +1,8 @@
 package com.bascomb;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,23 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        //Init
+        List<Integer> heights = Arrays.asList(25, 12, 37);
+
+        //Print Init State
+        heights.forEach(System.out::println);
+
+        //Print all heights less than 16;
+        System.out.println("\nSmall Dogs (<16): ");
+        heights.stream()
+                .filter(h -> h < 16)
+                .forEach(System.out::println);
+
+        //Print all heights ordered largest to smallest;
+        System.out.println("\n Shortest to Tallest: ");
+        heights.stream()
+                .sorted((a,b) -> a - b)
+                .forEach(System.out::println);
     }
 }
